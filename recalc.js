b67fabcf0ba6d16b9c238ec2d7bac0ea642e1cbf
Esshka <esshka@gmail.com>
2015-06-25 20:38:33 +0300

    update with streams

--- recalc.js
+++ recalc.js
@@ -1,54 +1,57 @@
 #!/usr/bin/env node
 'use strict';
 
-var Sequelize = require('sequelize');
-var db = new Sequelize('zoomator_new2', null, null, {
-  dialect: 'postgres',
-  logging: false
-});
-var _ = require('lodash');
+var pg = require('pg');
+var QueryStream = require('pg-query-stream');
+var conString = "postgres://localhost/zoomator_new2";
+var Writable = require('stream').Writable;
 var deviceId = process.argv[2];
+var util = require('util');
+var  _ = require('lodash');
 
-var Clamp = db.define('clamp', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  type: {
-    type: Sequelize.STRING
-  },
-  duration: {
-    type: Sequelize.STRING
-  },
-  time: {
-    type: Sequelize.DATE
-  },
-  device_id: {
-    type: Sequelize.INTEGER
-  },
-  updatedAt: {
-    field: 'updated_at',
-    type: Sequelize.DATE
-  }
-});
+var WriteStream = function() {
+  Writable.call(this, {objectMode: true});
+};
+util.inherits(WriteStream, Writable);
 
-Clamp.findAll({
-  attributes: ['id', 'time', 'type', 'duration'],
-  where: { device_id: deviceId },
-  order: '"time" ASC',
-})
-  .then(function(clamps) {
-      var total = clamps.length;
-      _.each(clamps, function(clamp, idx){
-        if(idx === 0) {
-          clamp.duration = 0;
-        } else if (idx != total) {
-          let next = clamps[idx+1];
-          clamp.duration = ((+new Date(clamps[idx+1].time)) - (+new Date(clamp.time))) / 1000;
-        } else {
-          clamp.duration = ((+new Date()) - (+new Date(clamp.time))) / 1000;
+pg.connect(conString, function(err, client, done) {
+    let counter = 0;
+    let prev;
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    var updateClampDuration = function(clampData){
+      console.log(`updating clamp #${clampData.id} with duration ${clampData.duration}`);
+      client.query(`UPDATE clamps SET duration = ${clampData.duration} WHERE device_id = ${clampData.device_id}`, function(err, result){
+        done();
+        if(err) {
+          return console.error('error running query', err);
         }
-        clamp.save();
       });
-  });
+      counter++;
+    }
+
+    WriteStream.prototype._write = function(chunk, encoding, callback) {
+      if (!prev) {
+        chunk.duration = 0;
+        updateClampDuration(chunk);
+      } else {
+        let updatedDuration = ((+new Date(chunk.time)) - (+new Date(prev.time))) / 1000;
+        chunk.duration = updatedDuration;
+        updateClampDuration(chunk);
+      }
+      prev = chunk;
+      callback();
+    };
+
+    let updateStream = new WriteStream();
+
+    let query = new QueryStream(`SELECT id,time,type,duration,device_id FROM clamps WHERE device_id = ${deviceId} ORDER BY time ASC`);
+    let readStream = client.query(query);
+    readStream.pipe(updateStream);
+
+    readStream.on('end', function(){
+      console.log('finished', counter, 'clamps updated');
+    });
+});
